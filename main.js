7c24d8d2e5947481e238bc383bc0f365074927e0
Jake Boyles <jake@jibdesigns.com>
2016-10-16 20:37:52 -0400

    inital commit

---
+++ main.js
@@ -0,0 +1,32 @@
+(function () {
+
+  // See if we can get our local IP to make it easier...
+  window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;   //compatibility for firefox and chrome
+  var pc = new RTCPeerConnection({iceServers:[]}), noop = function(){};      
+  pc.createDataChannel("");    //create a bogus data channel
+  pc.createOffer(pc.setLocalDescription.bind(pc), noop);    // create offer and set local description
+  pc.onicecandidate = function(ice){  //listen for candidate events
+      if(!ice || !ice.candidate || !ice.candidate.candidate)  return;
+      var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+      $("#ip").html(`<h4>Your IP is: ${myIP}</h4>`);  
+      pc.onicecandidate = noop;
+  };
+
+  function saySomething(text,name)
+  {
+    $.ajax({
+      method:"POST",
+      data:{text,name},
+      url: `http://192.168.200.24:3005/talkToMe`,
+      success: function successHandler(resp) {
+        console.log(resp);
+    }});
+  }
+
+  $("button").on("click",function(){
+    var text = $("#text").val();
+    var name = $("#name").val();
+    saySomething(text,name);
+  })
+
+})();
